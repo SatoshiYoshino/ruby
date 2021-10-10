d96cf8adb63631be5c1abdb587843a22540fd16c
Satoshi Yoshino <waterhanmer@gmail>
2021-10-10 19:41:40 +0900

    chapter2

---
+++ exercises.rb
@@ -0,0 +1,15 @@
+a = 2 + 3
+puts a
+
+b = 2 * 2 * 3.14
+puts b
+
+puts "abc" + "def"
+
+puts "123".to_i + "456".to_i
+
+
+cofee=400
+espresso=100
+puts "コーヒー: #{cofee}円"
+puts "合計： #{cofee + espresso * 2}"
